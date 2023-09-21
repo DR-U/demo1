2372411d5037415ca93c364ebde7b099f4ac1056
Divya Uike <145578571+DR-U@users.noreply.github.com>
2023-09-21 10:30:33 +0530

    Add files via upload

---
+++ variable r.R
@@ -0,0 +1,76 @@
+
+print ('Learning R')
+print (10)
+
+print <10*5
+
+a<-10*5
+
+a
+
+
+1 ten=10
+ten1 <- 5
+coursename <- 'DA'
+corse name <- 'DA'
+course_name <- 'DS'
+a! = 53
+
+rm(ten1)
+rm(coursename)
+
+
+
+
+
+
+
+ten = 10
+class(ten)
+
+f =10.2
+class(f)
+
+city <- 'pune'
+class(city)
+
+pin_code = '54614'
+class(pin_code)
+
+
+boolean <- TRUR
+class(boolean)
+
+
+age <- 20,21,25,26,24
+
+
+
+
+
+
+
+
+age <- c(20,21,25,26,23)
+
+info <- c('sakshi',25,145.32,TRUE)
+
+
+
+
+info [2]
+age[2]
+age [1:3]
+info[2:3]
+info[c(1.3)]
+
+
+
+
+
+
+
+info[2] <- '22'
+info 
+info [c(1,4)] <- c('Tarun','False')
+info
